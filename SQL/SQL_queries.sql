508f995a495995f48a7296aabda01a1486f8d036
Sergio Heynes <sergio.heynes@gmail.com>
2019-06-30 19:47:50 -0500

    added grid layout.

---
+++ SQL/SQL_queries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE properties (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    address_street VARCHAR(255) NOT NULL,
+    address_number INT NOT NULL,
+    address_subburb VARCHAR(255) NOT NULL,
+    house_image VARCHAR(255) NOT NULL
+);
+    
